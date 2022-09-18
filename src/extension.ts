c89189c1ccc8c0b2f185fca34ebf5ba6c58aa423
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-18 14:44:29 +0800

    Register blade formatter and create blade file command

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,106 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { Formatter } from "blade-formatter";
+import { TextEncoder } from "util";
+import * as vscode from "vscode";
+
+const path = require("path");
+const fs = require("fs");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "laravel-blade" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  // console.log('Congratulations, your extension "laravel-blade" is now active!');
+  vscode.window.showInformationMessage("Laravel Blade is now active!");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('laravel-blade.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Laravel Blade!');
-	});
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "laravel-blade.createBladeFile",
+      async () => {
+        const bladeFileName = await vscode.window.showInputBox({
+          prompt: "Enter blade file name using dot notation",
+          placeHolder: "example.hello-world",
+        });
 
-	context.subscriptions.push(disposable);
+        if (bladeFileName === "" || bladeFileName === undefined) {
+          return;
+        }
+
+        if (
+          vscode.workspace.workspaceFolders === undefined ||
+          vscode.workspace.workspaceFolders.length === 0
+        ) {
+          return;
+        }
+
+        const bladePath = bladeFileName.split(".");
+
+        const workspaceFsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+
+        const dirPath = path.join(
+          workspaceFsPath,
+          "resources/views",
+          bladePath.slice(0, bladePath.length - 1).join("/")
+        );
+
+        const fileName = `${bladePath[bladePath.length - 1]}.blade.php`;
+
+        const fullFilePath = path.join(dirPath, fileName);
+
+        if (fs.existsSync(fullFilePath)) {
+          vscode.window.showErrorMessage(
+            "File already exists. Please choose another name."
+          );
+          return;
+        }
+        const fileUri = vscode.Uri.file(fullFilePath);
+
+        await vscode.workspace.fs.writeFile(fileUri, new TextEncoder().encode(""));
+
+        vscode.window.showInformationMessage("File is created successfully.");
+        console.log("File path: " + fullFilePath + " File URI: " + fileUri);
+
+        vscode.workspace
+          .openTextDocument(fileUri)
+          .then((doc) => {
+            vscode.window.showTextDocument(doc);
+          });
+      }
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider("blade", {
+      provideDocumentFormattingEdits(document: vscode.TextDocument) {
+        const lastLine = document.lineAt(document.lineCount - 1);
+        const range = new vscode.Range(
+          new vscode.Position(0, 0),
+          lastLine.range.end
+        );
+
+        const options = {
+          indentSize: 4,
+          sortTailwindcssClasses: true,
+          noMultipleEmptyLines: true,
+          sortHtmlAttributes: "code-guide",
+          endWithNewLine: true,
+          wrapAttributes: "force-expand-multiline",
+        };
+
+        return new Formatter(options)
+          .formatContent(document.getText())
+          .then((formattedText: string) => {
+            return [new vscode.TextEdit(range, formattedText)];
+          })
+          .catch((error: Error) => {
+            vscode.window.showErrorMessage(error.message);
+            return [];
+          });
+      },
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
